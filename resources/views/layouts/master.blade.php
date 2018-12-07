437e7670d4cd3a576b6b2ad1efc0f9e5d66a4630
Babou Touray <babou2ray19@yahoo.com>
2018-12-06 18:28:26 -0800

    I have worked on the parties and areas

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -231,7 +231,7 @@
     <div class="pull-right hidden-xs">
       <b>Version</b> 1.0
     </div>
-    <strong>Copyright &copy; 2014-2016 <a href="https://adminlte.io">Babu Touray</a>.</strong> All rights
+    <strong>Copyright &copy; 2014-2016 <a href="https://adminlte.io">Babou Touray</a>.</strong> All rights
     reserved.
   </footer>
 
